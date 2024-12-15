ba780a51d690f9a81318d9f7631e8ec1046809a5
Remi Bernotavicius <remi@abort.cc>
2024-12-15 12:51:43 -0800

    Let SQL sort the ingredients for us

--- src/ui/ingredient_list.rs
+++ src/ui/ingredient_list.rs
@@ -1,13 +1,13 @@
 use crate::database;
 use crate::database::models::Ingredient;
+use diesel::ExpressionMethods as _;
 use diesel::QueryDsl as _;
 use diesel::RunQueryDsl as _;
 use diesel::SelectableHelper as _;
 use eframe::egui;
-use std::collections::BTreeMap;
 
 pub struct IngredientListWindow {
-    all_ingredients: BTreeMap<String, Ingredient>,
+    all_ingredients: Vec<Ingredient>,
 }
 
 impl IngredientListWindow {
@@ -15,14 +15,10 @@ impl IngredientListWindow {
         use database::schema::ingredients::dsl::*;
         let all_ingredients = ingredients
             .select(Ingredient::as_select())
+            .order_by(name.asc())
             .load(conn)
             .unwrap();
-        Self {
-            all_ingredients: all_ingredients
-                .into_iter()
-                .map(|i| (i.name.clone(), i))
-                .collect(),
-        }
+        Self { all_ingredients }
     }
 
     pub fn update(&mut self, _conn: &mut database::Connection, ctx: &egui::Context) -> bool {
@@ -36,7 +32,7 @@ impl IngredientListWindow {
                         ui.label("Category");
                         ui.end_row();
 
-                        for ingredient in self.all_ingredients.values() {
+                        for ingredient in &self.all_ingredients {
                             ui.label(&ingredient.name);
                             ui.label(ingredient.category.as_deref().unwrap_or(""));
                             ui.end_row();
