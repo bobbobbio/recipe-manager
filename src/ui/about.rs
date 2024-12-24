ba52ee98969b863701bf578a18affb2eb4e60df2
Remi Bernotavicius <remi@abort.cc>
2024-12-23 20:52:57 -0800

    Disallow resizing of about window

--- src/ui/about.rs
+++ src/ui/about.rs
@@ -8,22 +8,25 @@ impl AboutWindow {
     pub fn update(&mut self, ctx: &egui::Context) -> bool {
         let mut open = true;
 
-        egui::Window::new("About").open(&mut open).show(ctx, |ui| {
-            ui.vertical_centered(|ui| {
-                ui.add(
-                    egui::Image::new(egui::include_image!("../../images/appicon.png"))
-                        .maintain_aspect_ratio(true)
-                        .max_width(100.0),
-                );
-                ui.heading("Recipe Manager");
-                ui.label(format!("Version {}", env!("CARGO_PKG_VERSION")));
-                ui.label("Copyright Remi Bernotavicius 2024");
-                ui.hyperlink_to(
-                    "Code on GitHub",
-                    "https://github.com/bobbobbio/recipe-manager",
-                );
+        egui::Window::new("About")
+            .resizable([false, false])
+            .open(&mut open)
+            .show(ctx, |ui| {
+                ui.vertical_centered(|ui| {
+                    ui.add(
+                        egui::Image::new(egui::include_image!("../../images/appicon.png"))
+                            .maintain_aspect_ratio(true)
+                            .max_width(100.0),
+                    );
+                    ui.heading("Recipe Manager");
+                    ui.label(format!("Version {}", env!("CARGO_PKG_VERSION")));
+                    ui.label("Copyright Remi Bernotavicius 2024");
+                    ui.hyperlink_to(
+                        "Code on GitHub",
+                        "https://github.com/bobbobbio/recipe-manager",
+                    );
+                });
             });
-        });
 
         !open
     }
