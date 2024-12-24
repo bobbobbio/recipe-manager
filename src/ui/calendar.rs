8d2af1043092acd2b24c51067648f553654fe3fb
Remi Bernotavicius <remi@abort.cc>
2024-12-23 19:45:06 -0800

    Use table for calendar window

--- src/ui/calendar.rs
+++ src/ui/calendar.rs
@@ -152,114 +152,181 @@ impl CalendarWindow {
         }
     }
 
+    fn update_table(
+        &mut self,
+        conn: &mut database::Connection,
+        toasts: &mut egui_toast::Toasts,
+        body: &mut egui_extras::TableBody<'_>,
+    ) -> Vec<UpdateEvent> {
+        let mut events = vec![];
+        for (day, recipe) in self.week.recipes() {
+            body.row(20.0, |mut row| {
+                row.col(|ui| {
+                    ui.label(full_day_name(day));
+                });
+                if let Some(recipe) = recipe {
+                    row.col(|ui| {
+                        ui.label(recipe.name.clone());
+                    });
+                    row.col(|ui| {
+                        if self.edit_mode && ui.button("Clear").clicked() {
+                            self.week.clear_day(conn, day);
+                        }
+                    });
+                    row.col(|_| {});
+                } else {
+                    row.col(|ui| {
+                        ui.label("No Recipe");
+                    });
+                    if self.edit_mode {
+                        let entry = self.recipes_being_selected.entry(day).or_default();
+                        let mut selected = false;
+                        row.col(|ui| {
+                            selected |= ui
+                                .add(
+                                    SearchWidget::new(
+                                        ("calendar select recipe", day),
+                                        &mut entry.name,
+                                        &mut entry.recipe_id,
+                                        |query| {
+                                            query::search_recipes(
+                                                conn,
+                                                &mut entry.cached_recipe_search,
+                                                query,
+                                            )
+                                        },
+                                    )
+                                    .desired_width(ui.available_width())
+                                    .hint_text("search for recipe"),
+                                )
+                                .pressed_enter();
+                        });
+
+                        let e = !entry.name.is_empty();
+                        row.col(|ui| {
+                            selected |= ui.add_enabled(e, egui::Button::new("Select")).clicked();
+                        });
+
+                        if selected && e {
+                            if let Some(recipe_id) = entry.recipe_id {
+                                self.week.schedule(conn, day, recipe_id);
+                                *entry = Default::default();
+
+                                events.push(UpdateEvent::RecipeScheduled {
+                                    week: self.week.week().clone(),
+                                });
+                            } else {
+                                toasts.add(new_error_toast("Couldn't find recipe"));
+                            }
+                        }
+                    }
+                }
+            });
+        }
+        events
+    }
+
+    fn update_controls(
+        &mut self,
+        conn: &mut database::Connection,
+        toasts: &mut egui_toast::Toasts,
+        ui: &mut egui::Ui,
+    ) {
+        ui.separator();
+        ui.horizontal(|ui| {
+            ui.toggle_value(&mut self.edit_mode, "Edit");
+            if ui.button("Previous").clicked() {
+                self.week.previous(conn);
+                self.recipes_being_selected.clear();
+            }
+            if ui.button("Next").clicked() {
+                self.week.advance(conn);
+                self.recipes_being_selected.clear();
+            }
+            ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                if ui.button("Menu").clicked() {
+                    if let Err(error) = generate_rtf::generate_and_open_menu(&self.week) {
+                        toasts.add(new_error_toast(format!("Error generating menu: {error}")));
+                    }
+                }
+                if ui.button("Shopping List").clicked() {
+                    let mut ingredients = vec![];
+                    for (_, recipe) in self.week.recipes() {
+                        if let Some(recipe) = recipe {
+                            ingredients.extend(query::get_ingredients_for_recipe(conn, recipe.id));
+                        }
+                    }
+                    if let Err(error) =
+                        generate_rtf::generate_and_open_shopping_list(self.week.week(), ingredients)
+                    {
+                        toasts.add(new_error_toast(format!(
+                            "Error generating shopping list: {error}"
+                        )));
+                    }
+                }
+            });
+        });
+    }
+
     pub fn update(
         &mut self,
         ctx: &egui::Context,
         conn: &mut database::Connection,
         toasts: &mut egui_toast::Toasts,
     ) -> Vec<UpdateEvent> {
+        let style = ctx.style();
+        let text_height = egui::TextStyle::Body
+            .resolve(&style)
+            .size
+            .max(style.spacing.interact_size.y);
+        let button_height = (egui::TextStyle::Button.resolve(&style).size
+            + style.spacing.button_padding.y as f32 * 2.0)
+            .max(style.spacing.interact_size.y);
+        let spacing = style.spacing.item_spacing.y;
+        let separator_height = 6.0;
+
+        let title_height = text_height + spacing;
+        let controls_height = button_height + spacing + separator_height;
+
         let mut events = vec![];
         let mut open = true;
         egui::Window::new("Calendar")
             .open(&mut open)
+            .default_width(500.0)
+            .default_height(100.0)
             .show(ctx, |ui| {
-                egui::Grid::new("calendar grid").show(ui, |ui| {
-                    ui.label(format!("Week of "));
-                    self.week.pick_date(conn, |date| {
-                        ui.add(egui_extras::DatePickerButton::new(date));
+                egui_extras::StripBuilder::new(ui)
+                    .size(egui_extras::Size::exact(title_height))
+                    .size(egui_extras::Size::remainder())
+                    .size(egui_extras::Size::exact(controls_height))
+                    .vertical(|mut strip| {
+                        strip.cell(|ui| {
+                            ui.horizontal(|ui| {
+                                ui.label(format!("Week of "));
+                                self.week.pick_date(conn, |date| {
+                                    ui.add(egui_extras::DatePickerButton::new(date));
+                                });
+                            });
+                        });
+                        strip.cell(|ui| {
+                            egui::ScrollArea::horizontal().show(ui, |ui| {
+                                egui_extras::TableBuilder::new(ui)
+                                    .id_salt("calendar table")
+                                    .striped(false)
+                                    .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                                    .column(egui_extras::Column::exact(80.0))
+                                    .column(egui_extras::Column::remainder())
+                                    .column(egui_extras::Column::exact(200.0))
+                                    .column(egui_extras::Column::exact(50.0))
+                                    .body(|mut body| {
+                                        events.extend(self.update_table(conn, toasts, &mut body));
+                                    });
+                            });
+                        });
+                        strip.cell(|ui| {
+                            self.update_controls(conn, toasts, ui);
+                        });
                     });
-                    ui.end_row();
-
-                    for (day, recipe) in self.week.recipes() {
-                        ui.label(full_day_name(day));
-                        if let Some(recipe) = recipe {
-                            ui.label(recipe.name.clone());
-                            if self.edit_mode && ui.button("Clear").clicked() {
-                                self.week.clear_day(conn, day);
-                            }
-                        } else {
-                            ui.label("No Recipe");
-                            if self.edit_mode {
-                                let entry = self.recipes_being_selected.entry(day).or_default();
-                                let mut selected = false;
-                                selected |= ui
-                                    .add_sized(
-                                        egui::vec2(200.0, 15.0),
-                                        SearchWidget::new(
-                                            ("calendar select recipe", day),
-                                            &mut entry.name,
-                                            &mut entry.recipe_id,
-                                            |query| {
-                                                query::search_recipes(
-                                                    conn,
-                                                    &mut entry.cached_recipe_search,
-                                                    query,
-                                                )
-                                            },
-                                        )
-                                        .hint_text("search for recipe"),
-                                    )
-                                    .pressed_enter();
-
-                                let e = !entry.name.is_empty();
-                                selected |=
-                                    ui.add_enabled(e, egui::Button::new("Select")).clicked();
-
-                                if selected && e {
-                                    if let Some(recipe_id) = entry.recipe_id {
-                                        self.week.schedule(conn, day, recipe_id);
-                                        *entry = Default::default();
-
-                                        events.push(UpdateEvent::RecipeScheduled {
-                                            week: self.week.week().clone(),
-                                        });
-                                    } else {
-                                        toasts.add(new_error_toast("Couldn't find recipe"));
-                                    }
-                                }
-                            }
-                        }
-                        ui.end_row();
-                    }
-                });
-                ui.separator();
-                ui.horizontal(|ui| {
-                    ui.toggle_value(&mut self.edit_mode, "Edit");
-                    if ui.button("Previous").clicked() {
-                        self.week.previous(conn);
-                        self.recipes_being_selected.clear();
-                    }
-                    if ui.button("Next").clicked() {
-                        self.week.advance(conn);
-                        self.recipes_being_selected.clear();
-                    }
-                    ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                        if ui.button("Menu").clicked() {
-                            if let Err(error) = generate_rtf::generate_and_open_menu(&self.week) {
-                                toasts.add(new_error_toast(format!(
-                                    "Error generating menu: {error}"
-                                )));
-                            }
-                        }
-                        if ui.button("Shopping List").clicked() {
-                            let mut ingredients = vec![];
-                            for (_, recipe) in self.week.recipes() {
-                                if let Some(recipe) = recipe {
-                                    ingredients
-                                        .extend(query::get_ingredients_for_recipe(conn, recipe.id));
-                                }
-                            }
-                            if let Err(error) = generate_rtf::generate_and_open_shopping_list(
-                                self.week.week(),
-                                ingredients,
-                            ) {
-                                toasts.add(new_error_toast(format!(
-                                    "Error generating shopping list: {error}"
-                                )));
-                            }
-                        }
-                    });
-                });
             });
 
         if !open {
